ee6951660a889b6eefa18ef0a3718190f33863ba
dhritisxn <dhriti.saxena006@gmail.com>
2025-04-06 19:20:30 +0530

    fixed minor bugs

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import MessFeedbackForm from "./components/MessFeedbackForm";
+
+function App() {
+  return (
+    <div className=" min-vh-100 d-flex align-items-center justify-content-center">
+      <MessFeedbackForm />
+    </div>
+  );
+}
+
+export default App;
